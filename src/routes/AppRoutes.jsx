09038cdcf5af80f5a99e7095b0fa2e9c4e66ec00
YussefMo <youssefapoulila@gmail.com>
2025-06-27 11:40:22 +0300

    fix typo error

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import AppLayout from '../components/AppLayout';
 import { lazy } from 'react';
 
-
 const Home = lazy(() => import('../pages/Home'));
 const Blog = lazy(() => import('../pages/Blog/Blog'));
 const Contact = lazy(() => import('../pages/Contact'));
-const Shop = lazy(() => import("../components/Shop/Shop"))
-
+const Shop = lazy(() => import('../components/Shop/Shop'));
 
 // eslint-disable-next-line react-refresh/only-export-components
 export const router = createBrowserRouter([
@@ -18,11 +16,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <Home />
-      }, 
+      },
       {
-        path : "/shop" , 
-        element : <Shop />
-      }
+        path: '/shop',
+        element: <Shop />
       },
       {
         path: '/blog',
